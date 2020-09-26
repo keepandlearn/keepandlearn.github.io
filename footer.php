a0b065265874dece45e8fa7a7763016eaeed2b26
keepandlearn <71906584+keepandlearn@users.noreply.github.com>
2020-09-26 14:22:36 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Copyright © 2020 Keep & Learn Inc. All rights reserved.</p>
+</div>
---
+++ header.php
@@ -0,0 +1,4 @@
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>Keep & Learn</h1>
+  <p></p> 
+</div>
